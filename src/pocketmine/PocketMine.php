f5f540979e49e533882daeca28e4b1fcb10888b2
Apple <ytapplegamer@gmail.com>
2017-09-05 19:27:50 -0400

    Downgrade versions

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -72,9 +72,9 @@ namespace pocketmine {
 	use pocketmine\utils\Utils;
 	use pocketmine\wizard\Installer;
 
-	const VERSION = "1.2.0";
-	const API_VERSION = "3.0.1";
-	const CODENAME = "Ruby";
+	const VERSION = "1.1.0";
+	const API_VERSION = "2.0.1";
+	const CODENAME = "Diamond";
 
 	/*
 	 * Startup code. Do not look at it, it may harm you.
